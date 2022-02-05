3c6e1bc9ed6c501803b1969ec69b5bd2c2d3d70b
nehanazneen <80847809+nehanazneen@users.noreply.github.com>
2022-02-05 13:40:42 +0530

    Add files via upload
    WAP to convert string to primitive data types.

---
+++ stringtoprimitive.java
@@ -0,0 +1,22 @@
+package sttopri;
+
+public class stringtoprimitive {
+
+	public static void main(String[] args) {
+		
+		
+		
+		String s1 = "200";
+		String s2 = "100";
+		int a,b;
+
+		 a = Integer.parseInt(s1);
+		 b = Integer.parseInt(s2);
+		int sum;
+
+		sum = a+b;
+		System.out.println("Sum = " +sum);
+
+	}
+
+}
