fea06f267c30e9144ca9c7ba1c295a7353079fc8
nehanazneen <80847809+nehanazneen@users.noreply.github.com>
2022-02-11 14:37:02 +0530

    Add files via upload

---
+++ Collection/src/Collections/Arraylist.java
@@ -0,0 +1,17 @@
+package Collections;
+
+import java.util.*;
+
+public class Arraylist {
+	public static void main(String args[]) {
+		ArrayList<String> list = new ArrayList<String>();// Creating arraylist
+		list.add("Mango");// Adding object in arraylist
+		list.add("Apple");
+		list.add("Banana");
+		list.add("Grapes");
+
+		System.out.println(list);
+
+	}
+
+}
---
+++ Collection/src/Collections/Vectorlist.java
@@ -0,0 +1,20 @@
+package Collections;
+
+import java.util.*;  
+public class Vectorlist {  
+       public static void main(String args[]) {  
+         
+          Vector<String> vec = new Vector<String>();  
+          
+          vec.add("Tiger");  
+          vec.add("Lion");  
+          vec.add("Dog");  
+          vec.add("Elephant");  
+         
+          vec.addElement("Rat");  
+          vec.addElement("Cat");  
+          vec.addElement("Deer");  
+            
+          System.out.println("Elements are: "+vec);  
+       }  
+}  
